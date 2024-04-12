d5f189f8cc1f025acc76d0ec68b8eca752170004
Kris <isaevachristin@gmail.com>
2024-04-12 18:46:28 +0300

    добавила код

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[] array = ["Hello", "2", "world", ":-)"];
+string[] array_2 = [];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <=3)
+    {
+
+Array.Resize(ref array_2, array_2.Length+1);
+array_2[array_2.Length-1] = array[i];
+    }
+}
+foreach ( string j in array_2)
+{
+    Console.Write($"{j} ");
+}
